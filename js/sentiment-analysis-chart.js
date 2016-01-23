6ed44e0257ad81b02d69243326be75ac04f3b553
conrad <woosys25@gmail.com>
2016-01-23 15:06:32 +0900

    test
    Signed-off-by: conrad <woosys25@gmail.com>

--- js/sentiment-analysis-chart.js
+++ js/sentiment-analysis-chart.js
@@ -1,42 +1,93 @@
-'use strict';
+var sentimentChart = dc.seriesChart("#sentiment-line-chart");
+var ndx, runDimension, runGroup;
 
-var dayOfWeekChart = dc.rowChart('#sentiment-chart');
+d3.csv("../js/morcy.csv", function(error, experiments) {
 
-//d3.json('data.json', function(data) {...};
-d3.csv('../js/sentiment.csv', function (data) {
+    ndx = crossfilter(experiments);
+    runDimension = ndx.dimension(function(d) {return [+d.Expt, +d.Run]; });
+    runGroup = runDimension.group().reduceSum(function(d) { return +d.Speed; });
 
-    data.forEach(function (d) {
-        d.sentiment = +d.sentiment;
-    });
-
-    var ndx = crossfilter(data);
-
-    var sentimentChartWidth = document.getElementById("sentiment-chart").clientWidth;
-
-    var dayOfWeek = ndx.dimension(function (d) {
-        var day = d.sentiment;
-        var name = ['Basic Positive', 'Negative', 'Basic Neutral'];
-        return day + '.' + name[day];
-    });
-
-    var dayOfWeekGroup = dayOfWeek.group();
-
-    dayOfWeekChart /* dc.rowChart('#day-of-week-chart', 'chartGroup') */
-        .width(sentimentChartWidth)
-        .height(180)
-        .margins({top: 20, left: 10, right: 10, bottom: 20})
-        .group(dayOfWeekGroup)
-        .dimension(dayOfWeek)
-        .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
-        .label(function (d) {
-            return d.key.split('.')[1];
-        })
-        .title(function (d) {
-            return d.value;
-        })
-        .elasticX(true)
-        .xAxis().ticks(4)
+    sentimentChart
+        .width(768)
+        .height(480)
+        .chart(function(c) { return dc.lineChart(c).interpolate('basis'); })
+        .x(d3.scale.linear().domain([0,20]))
+        .brushOn(false)
+        .yAxisLabel("Measured Speed km/s")
+        .xAxisLabel("Run")
+        .clipPadding(10)
+        .elasticY(true)
+        .renderHorizontalGridLines(true)
+        .dimension(runDimension)
+        .group(runGroup)
+        .mouseZoomable(true)
+        .seriesAccessor(function(d) {return "Expt: " + d.key[0];})
+        .keyAccessor(function(d) {return +d.key[1];})
+        .valueAccessor(function(d) {return +d.value - 500;})
+        .legend(dc.legend().x(350).y(350).itemHeight(13).gap(5).horizontal(1).legendWidth(140).itemWidth(70));
+    sentimentChart.yAxis().tickFormat(function(d) {return d3.format(',d')(d+299500);});
+    sentimentChart.margins().left += 40;
 
     dc.renderAll();
 
 });
+
+function load_button(file) {
+    return function load_it() {
+        d3.csv(file, function(error, experiments) {
+            ndx.remove();
+            ndx.add(experiments);
+            dc.redrawAll();
+        });
+    };
+}
+
+var button1 = load_button("morley.csv"),
+    button2 = load_button("morley2.csv"),
+    button3 = load_button("morley3.csv");
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var data = [
+    {Id: "01", Name: "Red", Price: "1.00", Quantity: "1",TimeStamp:111},
+    {Id: "04", Name: "Blue", Price: "9.50", Quantity: "8",TimeStamp:434},
+    {Id: "05",Name: "Blue", Price: "1.20", Quantity: "1",TimeStamp:777}
+];
+
+
+var color = d3.scale.category20();
+
+var chart = d3.select("#sentiment-chart")
+
+var bar = chart.selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .attr('data-tooltip',function(d,i){ return d.Name} )
+    .attr('style',function(d,i){
+        return (
+        'flex:' + d.Quantity + '; '
+        + 'background:' + color(i) + ';'
+        )
+    })
+    .append("div")
+    .attr('style',function(d,i){
+        return (
+        'margin-top:40px;text-align:center'
+        )
+    })
+    .text(function(d){
+        return (
+            "ok 10%"
+        )
+    })
+    .on("click",function(d,i){
+        //updateElements(data)
+        d3.select("#rowChart")
+            .selectAll("div")
+            .attr("class", function(e, j) { return j != i ? "deselected" : "selected";
+            });
+    })
+
